7543dec641fa86af978c6460caed428a4ff3d5a7
xyz607xx <xyz607xx@gmail.com>
2018-01-17 00:39:33 +0800

    第一次提交

---
+++ 爬蟲/106年(2017)大學科技大學四技二專-榜單查詢/score.py
@@ -0,0 +1,68 @@
+from selenium import webdriver
+import os
+driverPath = 'D:\geckodriver\chromedriver.exe'
+browser = webdriver.Chrome(driverPath)
+url = 'https://www.com.tw/techreg/'
+browser.get(url)
+dataLen = len(browser.find_elements_by_css_selector('.schoolid~a'))
+titleDict,dictIndex = {},1
+for i in range(dataLen):
+    data = list(browser.find_elements_by_css_selector('.schoolid~a'))
+    key = data[i].text
+    titleDict[dictIndex]={key:{}}
+    data[i].click()
+    itemLen = len(browser.find_elements_by_css_selector('#table1 tr>td#university_dep_row_height:nth-child(2)'))
+    className = list(browser.find_elements_by_css_selector('#table1 tr>td#university_dep_row_height:nth-child(2)'))
+    classNumber = list(browser.find_elements_by_css_selector('#table1 tr>td#university_dep_row_height:nth-child(4)'))
+    classScore = list(browser.find_elements_by_css_selector('#table1 tr>td#university_dep_row_height:nth-child(5)'))
+    for j in range(0,itemLen,2):
+        tempKey = classNumber.pop(0).text
+        titleDict[dictIndex][key].setdefault(tempKey,{})
+        titleDict[dictIndex][key][tempKey][className[j].text] = classScore[int(j/2)].text
+    backObj = browser.find_element_by_link_text('回其它學校')
+    backObj.click()
+    dictIndex+=1
+browser.quit()
+numberSet = list(titleDict.keys())
+schoolSet = list(map(lambda x: list(x.keys()).pop(), titleDict.values()))
+printTitle = ''
+for i in range(len(numberSet)):
+    printTitle += str(numberSet[i])+':'+schoolSet[i]+'\n'
+def init():
+    while True:
+        print(printTitle)
+        index = int(input('請輸入要查詢的學校編號:\n'))
+        i = 1
+        classDict,classDictRecord,classDictTitle = {},{},{}
+        print(schoolSet[index-1]+'有以下群組:\n')
+        for key,value in list(titleDict[index].values()).pop().items():
+            tempKey = key[:2]
+            classDictTitle[int(tempKey)]=key[2:]
+            classDict.setdefault(int(tempKey),{})
+            classDictRecord.setdefault(int(tempKey),{})
+            classDictRecord[int(tempKey)]=key[2:]
+            classDict[int(tempKey)] = value
+        a,b=(list(t) for t in zip(*sorted(zip(classDictTitle.keys(), classDictTitle.values()))))
+        printGroupTitle = ""
+        for i in range(len(a)):
+            printGroupTitle+= str(a[i])+":"+str(b[i]+'\n')
+        print(printGroupTitle)
+        queryIndex = int(input('請輸入要查詢的群組編號:\n'))
+        classTitle = list(classDict[queryIndex].keys())
+        classScore = list(classDict[queryIndex].values())
+        classKey,printClassTitle = [],""
+        for i in range(len(classTitle)):
+            classKey.append(i+1)
+            printClassTitle += str(i+1)+":"+classTitle[i]+"\n"
+        print(schoolSet[index-1]+"的"+classDictRecord[queryIndex]+'群有以下科系:\n'+printClassTitle)
+        queryClassIndex = int(input("請輸入要查詢的科系編號:\n"))
+        print(schoolSet[index-1]+' '+ classDictRecord[queryIndex] +'群的' + classTitle[queryClassIndex-1] + '需要'+str(classScore[queryClassIndex-1])+'分')
+        os.system('pause')
+        os.system("cls")
+while True:
+    try:
+        os.system("cls")
+        init()
+    except Exception:
+        print('發生錯誤!請重試!')
+        os.system('pause')
