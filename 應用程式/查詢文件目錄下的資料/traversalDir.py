6e80274d2e00f86e06f014ee927f8dd5d62a6517
xyz607xx <xyz607xx@gmail.com>
2018-01-20 11:51:54 +0800

    增加基本錯誤處理

--- 應用程式/查詢文件目錄下的資料/traversalDir.py
+++ 應用程式/查詢文件目錄下的資料/traversalDir.py
@@ -1,22 +1,30 @@
 import os
+import sys
 while True:
+    errorFile = ''
     target = {
         "fileName" : input('請輸入要搜尋的關鍵字:\n'),
         "fileType" : input('請輸入要搜尋檔案的副檔名:\n')
     }
     record = {}
     def traversal(dirName):
-        record.setdefault(dirName,[])
-        for d in os.listdir(dirName):
-            fileName,fileType = os.path.splitext(d)
-            if os.path.isdir(dirName+fileName):
-                nextDir = dirName+fileName+'/'
-                traversal(nextDir)
-            elif fileType.find(target['fileType']) != -1:
-                with open(dirName+d,mode='r',encoding='utf8') as fileObj:
-                    jsText = str(fileObj.read())
-                    if jsText.find(target['fileName'])!=-1:
-                        record[dirName].append(d)
+        try:
+            record.setdefault(dirName,[])
+            for d in os.listdir(dirName):
+                errorFile = d
+                fileName,fileType = os.path.splitext(d)
+                if os.path.isdir(dirName+fileName):
+                    nextDir = dirName+fileName+'/'
+                    traversal(nextDir)
+                elif fileType.find(target['fileType']) != -1:
+                    with open(dirName+d,mode='r',encoding='utf8') as fileObj:
+                        jsText = str(fileObj.read())
+                        if jsText.find(target['fileName'])!=-1:
+                            record[dirName].append(d)
+        except Exception:
+            print('這個目錄下有存在Python無法開啟的資料夾或檔案，按下任意鍵結束程式')
+            os.system('pause')
+            sys.exit()
     traversal('./')
     result = ""
     for key,value in record.items():
