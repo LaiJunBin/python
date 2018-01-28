6c2d3cf396d8a360cf91d749f69facb04cf78a44
xyz607xx <xyz607xx@gmail.com>
2018-01-28 22:39:44 +0800

    編譯過的壓縮檔，可解壓縮後直接執行

--- 爬蟲/天瓏網路書店/web.py
+++
@@ -1,6 +0,0 @@
-from bs4 import BeautifulSoup as bs
-import requests as req
-r = req.get('https://www.tenlong.com.tw/')
-soup = bs(r.content)
-linkList = soup.select('.sidebox .link-list li a')
-linkList.pop(0)
